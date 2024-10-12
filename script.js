5bd119f9ee818adf5f1fb689a7bedb1d542f0412
Paulus Harman <paulus.otto.harman@gmail.com>
2024-10-13 00:02:08 +0700

    revisi skill dan penambahan loader/spinner

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const themeSelector = document.getElementById('theme-selector')
+const loader = document.getElementById('loader')
+const skillsContainer = document.getElementById('skills')
 
 themeSelector.addEventListener('change', event => {
     console.log(document.querySelector('select#theme-selector option:checked').value)
@@ -13,6 +15,7 @@ window.onhashchange = function() {
 
     switch (location.hash) {
         case '#skill':
+            show('loader')
             loadSkills()
             break;
     }
@@ -24,9 +27,32 @@ function loadSkills() {
         method:'get'
     }).then(async function(response) {
         const { data:skills } = await response.json()
-        skills.forEach(skill => {
+        skillsContainer.textContent = ''
+        skills.forEach( ({ skill, level }) => {
+            console.log(skill)
+            console.log(level)
+            const skillName = document.createElement('h4')
+            skillName.textContent = skill
+            const skillLevel = document.createElement('div')
+            skillLevel.classList.add('progress-bar', `level-${level}`)
 
+            const mySkill = document.createElement('article')
+            mySkill.appendChild(skillName)
+            mySkill.appendChild(skillLevel)
+
+            skillsContainer.appendChild(mySkill)
         })
+        hide('loader')
     })
+}
+
+function hide(elementId) {
+    const element = document.getElementById(elementId)
+    element.classList.add('hidden')
+}
+
+function show(elementId) {
+    const element = document.getElementById(elementId)
+    element.classList.remove(['hidden'])
 }
 
