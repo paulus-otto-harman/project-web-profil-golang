9013a6d301afa3f885fdc1488b7ca826cd4392b7
Paulus Harman <paulus.otto.harman@gmail.com>
2024-10-12 18:36:02 +0700

    revisi section contact dan dark mode

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// const navLinks = document.querySelectorAll('nav li a')
 const themeSelector = document.getElementById('theme-selector')
 
 themeSelector.addEventListener('change', event => {
@@ -11,4 +10,23 @@ window.onhashchange = function() {
     sections.forEach(section => section.classList.add('hidden'))
     const section = document.querySelector(location.hash)
     section.classList.remove('hidden')
+
+    switch (location.hash) {
+        case '#skill':
+            loadSkills()
+            break;
+    }
 }
+
+function loadSkills() {
+    const baseURL = 'https://paulus.free.beeceptor.com'
+    fetch(`${baseURL}/skills`, {
+        method:'get'
+    }).then(async function(response) {
+        const { data:skills } = await response.json()
+        skills.forEach(skill => {
+
+        })
+    })
+}
+
